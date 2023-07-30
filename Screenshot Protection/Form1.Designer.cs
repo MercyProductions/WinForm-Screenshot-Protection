ee344baf01a8cb4fe388d7c8a2fd878e8df4f84d
Mercy <102880099+MercyProductions@users.noreply.github.com>
2023-07-29 20:00:41 -0500

    Add files via upload

---
+++ Screenshot Protection/Form1.Designer.cs
@@ -0,0 +1,93 @@
+﻿
+namespace Screenshot_Protection
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.checkBoxScreenshotProtection = new System.Windows.Forms.CheckBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // checkBoxScreenshotProtection
+            // 
+            this.checkBoxScreenshotProtection.AutoSize = true;
+            this.checkBoxScreenshotProtection.Location = new System.Drawing.Point(12, 32);
+            this.checkBoxScreenshotProtection.Name = "checkBoxScreenshotProtection";
+            this.checkBoxScreenshotProtection.Size = new System.Drawing.Size(131, 17);
+            this.checkBoxScreenshotProtection.TabIndex = 0;
+            this.checkBoxScreenshotProtection.Text = "Screenshot Protection";
+            this.checkBoxScreenshotProtection.UseVisualStyleBackColor = true;
+            this.checkBoxScreenshotProtection.CheckedChanged += new System.EventHandler(this.checkBoxScreenshotProtection_CheckedChanged);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Location = new System.Drawing.Point(13, 152);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(395, 100);
+            this.groupBox1.TabIndex = 1;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Additional Information";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(6, 26);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(97, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Panic Hotkey = Ins\r\n";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(420, 270);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.checkBoxScreenshotProtection);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.CheckBox checkBoxScreenshotProtection;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ Screenshot Protection/Form1.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Diagnostics;
+using System.Runtime.InteropServices;
+using System.Windows.Forms;
+
+namespace Screenshot_Protection
+{
+    public partial class Form1 : Form
+    {
+        [DllImport("user32.dll")]
+        private static extern bool SetWindowDisplayAffinity(IntPtr hWnd, int dwAffinity);
+
+        private const int WDA_NONE = 0x00;
+        private const int WDA_MONITOR = 0x01;
+        private const int WH_KEYBOARD_LL = 13;
+        private const int WM_KEYDOWN = 0x0100;
+        private static LowLevelKeyboardProc _proc = HookCallback;
+        private static IntPtr _hookID = IntPtr.Zero;
+
+        private delegate IntPtr LowLevelKeyboardProc(int nCode, IntPtr wParam, IntPtr lParam);
+
+        [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        private static extern IntPtr SetWindowsHookEx(int idHook, LowLevelKeyboardProc lpfn, IntPtr hMod, uint dwThreadId);
+
+        [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        private static extern IntPtr CallNextHookEx(IntPtr hhk, int nCode, IntPtr wParam, IntPtr lParam);
+
+        [DllImport("kernel32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        private static extern IntPtr GetModuleHandle(string lpModuleName);
+
+        private static IntPtr HookCallback(int nCode, IntPtr wParam, IntPtr lParam)
+        {
+            if (nCode >= 0 && wParam == (IntPtr)WM_KEYDOWN)
+            {
+                int vkCode = Marshal.ReadInt32(lParam);
+                // Check if the pressed key is "Insert" (key code 45)
+                if ((Keys)vkCode == Keys.Insert)
+                {
+                    // Get the reference to form1
+                    Form1 form1 = (Form1)Application.OpenForms[0];
+
+                    // Check the form1's current transparency
+                    bool isTransparent = form1.Opacity == 0;
+
+                    // Toggle the transparency
+                    form1.Opacity = isTransparent ? 1.0 : 0;
+                }
+            }
+
+            return CallNextHookEx(_hookID, nCode, wParam, lParam);
+        }
+
+        private static IntPtr SetHook(LowLevelKeyboardProc proc)
+        {
+            using (ProcessModule module = Process.GetCurrentProcess().MainModule)
+            {
+                return SetWindowsHookEx(WH_KEYBOARD_LL, proc, GetModuleHandle(module.ModuleName), 0);
+            }
+        }
+
+        [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        private static extern bool UnhookWindowsHookEx(IntPtr hhk);
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void checkBoxScreenshotProtection_CheckedChanged(object sender, EventArgs e)
+        {
+            if (checkBoxScreenshotProtection.Checked)
+            {
+                // Enable screenshot protection
+                SetWindowDisplayAffinity(this.Handle, WDA_MONITOR);
+            }
+            else
+            {
+                // Disable screenshot protection
+                SetWindowDisplayAffinity(this.Handle, WDA_NONE);
+            }
+        }
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            checkBoxScreenshotProtection.CheckedChanged += checkBoxScreenshotProtection_CheckedChanged;
+            _hookID = SetHook(_proc);
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            UnhookWindowsHookEx(_hookID);
+            Application.Exit();
+        }
+    }
+}
